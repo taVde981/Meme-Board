6f5ceabbc54d2584451550e6997911062ebbcde7
taVde981 <124488640+taVde981@users.noreply.github.com>
2024-12-11 19:00:38 +0530

    Add files via upload

---
+++ playAudio.js
@@ -0,0 +1,99 @@
+$(".juicePilaDo").on("click", function(){
+  var audio = new Audio("sounds/juicePilaDo.mp3");
+    audio.play();
+});
+
+$(".hamsterEmo").on("click", function(){
+  var audio = new Audio("sounds/hamsterEmo.mp3");
+    audio.play();
+});
+
+$(".khaoMaKasam").on("click", function(){
+  var audio = new Audio("sounds/khaoMaKasam.mp3");
+    audio.play();
+});
+
+$(".systemPhadDengey").on("click", function(){
+  var audio = new Audio("sounds/systemPhadDengey.mp3");
+    audio.play();
+});
+
+$(".nagarPalika").on("click", function(){
+  var audio = new Audio("sounds/nagarPalika.mp3");
+    audio.play();
+});
+
+$(".majaNahi").on("click", function(){
+  var audio = new Audio("sounds/majaNahi.mp3");
+    audio.play();
+});
+
+$(".chadi").on("click", function(){
+  var audio = new Audio("sounds/chadi.mp3");
+    audio.play();
+});
+
+$(".mudra").on("click", function(){
+  var audio = new Audio("sounds/mudra.mp3");
+    audio.play();
+});
+
+$(".bihari").on("click", function(){
+  var audio = new Audio("sounds/bihari.mp3");
+    audio.play();
+});
+
+$(".tahalka").on("click", function(){
+  var audio = new Audio("sounds/tahalka.mp3");
+    audio.play();
+});
+
+$(".ghachar").on("click", function(){
+  var audio = new Audio("sounds/ghachar.mp3");
+    audio.play();
+});
+
+$(".puneet").on("click", function(){
+  var audio = new Audio("sounds/puneet.mp3");
+    audio.play();
+});
+
+$(".ammaBahan").on("click", function(){
+  var audio = new Audio("sounds/ammaBahan.mp3");
+    audio.play();
+});
+
+$(".elvishBhai").on("click", function(){
+  var audio = new Audio("sounds/elvishBhai.mp3");
+    audio.play();
+});
+
+$(".katappa").on("click", function(){
+  var audio = new Audio("sounds/katappa.mp3");
+    audio.play();
+});
+
+$(".power").on("click", function(){
+  var audio = new Audio("sounds/power.mp3");
+    audio.play();
+});
+
+$(".chhi").on("click", function(){
+  var audio = new Audio("sounds/chhi.mp3");
+    audio.play();
+});
+
+$(".ravan").on("click", function(){
+  var audio = new Audio("sounds/ravan.mp3");
+    audio.play();
+});
+
+$(".basKarBhai").on("click", function(){
+  var audio = new Audio("sounds/basKarBhai.mp3");
+    audio.play();
+});
+
+$(".aisaMatKaro").on("click", function(){
+  var audio = new Audio("sounds/aisaMatKaro.mp3");
+    audio.play();
+});
